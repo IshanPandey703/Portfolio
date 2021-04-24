37a8071196977993459a29317603a1d5c288ab51
IshanPandey703 <BTECH10703.20@bitmesra.ac.in>
2021-04-24 13:01:30 +0530

    Completed Landing Page

--- app.js
+++ app.js
@@ -21,5 +21,71 @@ const navSlide= () => {
         burger.classList.toggle('toggle');
     });
 }
-
 navSlide();
+
+const coolTextContainer = document.getElementById("typing");
+
+let i = 0;
+const coolText = [
+    "P",
+    "Pr",
+    "Pro",
+    "Prog",
+    "Progr",
+    "Progra",
+    "Program",
+    "Programm",
+    "Programme",
+    "Programmer",
+    "Programmer",
+    "Programmer",
+    "Programme",
+    "Programm",
+    "Program",
+    "Progra",
+    "Progr",
+    "Prog",
+    "Pro",
+    "Pr",
+    "P",
+    "D",
+    "De",
+    "Dev",
+    "Deve",
+    "Devel",
+    "Develo",
+    "Develop",
+    "Develope",
+    "Developer",
+    "Developer",
+    "Developer",
+    "Develope",
+    "Develop",
+    "Develo",
+    "Devel",
+    "Deve",
+    "Dev",
+    "De",
+    "D",
+    "L",
+    "Le",
+    "Lea",
+    "Lear",
+    "Learn",
+    "Learne",
+    "Learner",
+    "Learner",
+    "Learner",
+    "Learne",
+    "Learn",
+    "Lear",
+    "Lea",
+    "Le",
+    "L"
+];
+function coolTextHandler(){
+    coolTextContainer.innerHTML = coolText[i];
+    i = (i+1)%55;
+}
+
+setInterval(coolTextHandler, 250);
